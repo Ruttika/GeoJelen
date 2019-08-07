07aa4d68fc7995beca2a3acf7e6f9958ab3b60e0
Ruttika <woodo582@gmail.com>
2019-08-07 19:37:07 +0200

    init

---
+++ GeoJelen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoJelen
+//
+//  Created by Rudolf Dani on 2019. 08. 07..
+//  Copyright © 2019. Rudolf Dani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
